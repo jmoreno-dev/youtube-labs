d0f6410a0101a52d4c02bb38516cdc87e34e987d
José Antonio Moreno Marín <ytanonim12@gmail.com>
2026-09-24 20:13:39 +0200

    refactor: rename template

---
+++ 000-template/src/main/java/com/josemorenodevs/template/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.josemorenodevs.template.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.josemorenodevs.template.entity.User;
+import com.josemorenodevs.template.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.create(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.update(id, user);
+    }
+
+    @PatchMapping("/{id}")
+    public User patchUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.patch(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.delete(id);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+}
